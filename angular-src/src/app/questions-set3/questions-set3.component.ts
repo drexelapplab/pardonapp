2730e8d04ba4469e3fa064879aeace2cac84b3ce
Ryan Won <rhw37@drexel.edu>
2018-08-01 12:49:49 -0400

    layout update

--- angular-src/src/app/questions-set3/questions-set3.component.ts
+++ angular-src/src/app/questions-set3/questions-set3.component.ts
@@ -6,4 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: [ 'questions-set3.component.css' ]
 })
 export class QuestSet3Component  {
+  name = 'Angular 6';
+  marked = false;
+  caucasian = false;
+  asian = false;
+  aa = false;
+  pi = false;
+  na = false;
+  me = false;
+  constructor() {
+  }
+
+  toggleVisibility(e){
+    this.marked= e.target.checked;
+  }
 }
